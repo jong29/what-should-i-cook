192054f821e0958c3038c50b92489bee7bb8d4ef
michaelp3 <chan-park@hotmail.com>
2022-10-31 23:03:20 +0900

    Prototype Server done

--- Backend/src/main/java/WhatShouldICook/Backend/model/Tutorial.java
+++ Backend/src/main/java/WhatShouldICook/Backend/model/Tutorial.java
@@ -28,4 +28,44 @@ public class Tutorial {
     public Tutorial() {
 
     }
+
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isPublished() {
+        return published;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPublished(boolean published) {
+        this.published = published;
+    }
+
+    @Override
+    public String toString() {
+        return "Tutorial [" + "id=" + id + ", title='" + title + '\'' + ", description='"
+            + description + '\'' + ", published=" + published + ']';
+    }
 }
